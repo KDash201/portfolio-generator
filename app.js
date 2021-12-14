872839d4af0a4bca001798066f37811b59f6e9ea
KDash201 <KVonMadison@Yahoo.com>
2021-12-13 22:52:48 -0500

    node.js intro

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2)
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-// This...
-for (let i = 0; i < profileDataArr.length; i += 1) {
+const printProfileData = (profileDataArr) => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
     console.log(profileDataArr[i]);
   }
 
-  console.log('================');
+  console.log("================");
 
   // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-
+  profileDataArr.forEach((profileItem) => console.log(profileItem));
 };
-  
-  printProfileData(profileDataArgs);
+
+printProfileData(profileDataArgs);
